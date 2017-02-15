d7c7061ad14c4550dc1e9f9724d75ef2b2a97c7e
Ines Brotto <ines@onda.com.br>
2017-02-14 22:32:44 -0200

    Teste do Fibonacci de 1

--- src/devcast/palestra/git/Fibonacci.java
+++ src/devcast/palestra/git/Fibonacci.java
@@ -1,7 +1,11 @@
 package devcast.palestra.git;
 
 public class Fibonacci {
-		public int calcularFibonacci(int numero){
+	public int calcularFibonacci(int numero) {
+		if (numero == 0) {
 			return 0;
+		} else {
+			return 1;
 		}
+	}
 }
